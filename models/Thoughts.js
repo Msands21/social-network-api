46aec9ee09572aca34398b1bc8e83cf488fb955c
Mike Sands <mikesands94@gmail.com>
2022-05-23 21:39:41 -0400

    added reaction schema to thoughts file to fix model in array error

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,6 +1,33 @@
 const { Schema, model } = require('mongoose');
-const ReactionSchema = require('./Reaction');
 const dateFormat = require('../utils/dateFormat');
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 300
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timeStamp => dateFormat(timeStamp)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
 
 const ThoughtsSchema = new Schema(
     {
